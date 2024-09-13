d43f4ad71a43f3889d2d27550f57da6ed67eba34
Anuj Singh <108325005+Anujsingh2002@users.noreply.github.com>
2024-09-13 15:20:39 +0530

    Update CreateBooks.jsx

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -21,7 +21,7 @@ const CreateBooks = () => {
     };
     setLoading(true);
     axios
-      .post('http://localhost:5555/books', data)
+      .post('https://bookstore-backend-xszd.onrender.com/books', data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Created successfully', { variant: 'success' });
